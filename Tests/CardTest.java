576a8a99d8e45cbd8519fd99592f9fb168dd5cbd
ehokkanen <ellen.hokkanen@gmail.com>
2022-02-27 15:52:57 -0500

    Add Card Class

---
+++ Tests/CardTest.java
@@ -0,0 +1,12 @@
+import csc439teamcardvarks.cardgame.Card;
+import static com.google.common.truth.Truth.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+
+  @Test
+  void suitTest(){
+    Card ace = new Card("Club", "Ace");
+    assertThat(ace.getSuit()).isEqualTo("Club");
+  }
+}
--- src/csc439teamcardvarks/cardgame/Card.java
+++ src/csc439teamcardvarks/cardgame/Card.java
@@ -3,4 +3,17 @@ package csc439teamcardvarks.cardgame;
 public class Card {
   private String suit;
   private String number;
+
+  public Card(String suit, String number){
+    this.suit = suit;
+    this.number = number;
+  }
+
+  public String getSuit(){
+    return suit;
+  }
+
+  public String getNumber(){
+    return number;
+  }
 }
