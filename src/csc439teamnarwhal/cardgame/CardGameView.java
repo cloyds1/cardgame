28e2922770b0b7f0062e322cb618ce79ee5dd153
ehokkanen <ellen.hokkanen@gmail.com>
2022-04-08 12:22:40 -0400

    CardGameView update

--- src/csc439teamnarwhal/cardgame/CardGameView.java
+++ src/csc439teamnarwhal/cardgame/CardGameView.java
@@ -3,17 +3,19 @@ package csc439teamnarwhal.cardgame;
 import java.util.Scanner;
 
 public class CardGameView {
-
     private final Scanner scanner;
     private String text;
 
     public CardGameView() {
-        scanner = new Scanner(System.in);
+        this.scanner = new Scanner(System.in);
     }
+
     public void setText(String text) {
         System.out.println(text);
         this.text = text;
     }
 
-    public String getText() { return text; }
+    public String getText() {
+        return this.text;
+    }
 }
