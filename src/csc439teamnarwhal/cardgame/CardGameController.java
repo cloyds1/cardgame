b1fae6cfe4cc527c05430123c9d5dc1541ced558
ehokkanen <ellen.hokkanen@gmail.com>
2022-04-08 12:24:19 -0400

    CardGameView update

--- src/csc439teamnarwhal/cardgame/CardGameController.java
+++ src/csc439teamnarwhal/cardgame/CardGameController.java
@@ -1,5 +1,19 @@
 package csc439teamnarwhal.cardgame;
 
 public class CardGameController {
+  CardGameView view = new CardGameView();
 
 }
+ /* CountModel model = new CountModel(0);
+  CountView view = new CountView();
+  public void askForInput() {
+    view.setText("What color is the sky?");
+    view.getText();
+    model.setCount(model.getCount() + 1);
+  }
+  public void displayCount() {
+    view.setText("The number of times you answered is " + model.getCount());
+  }
+  public boolean canAskAgain() {
+    return model.getCount() < 3;
+}*/
